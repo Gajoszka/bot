d1679df4a6ce10eb5fb0bca86e565ed9d5a87604
Agata Gaj (gaja2) <gaja2@coventry.ac.uk>
2019-11-10 13:40:16 +0000

    10/11/2019 14:40

--- menu.py
+++ menu.py
@@ -1,29 +1,34 @@
-from actions import *
+from options import *
+from event import menuPosition
 from weather import weatherURL
 
-def addTitle():
-    title()
+def showMenu(events):
+    menuOptions = ["Add title", "Set start and end date", "Invite people", "Add description", "Check weather", "Finish and create an event", "Exit"]
+    run = True
+    while run:
+        print("----------------------------------------")
+        choice = menuPosition("Menu", menuOptions, "Please choose option: ")
 
-def setDates():
-    dates()
-
-def addPeople():
-    attendees()
-
-def addDescription():
-    description()
-
-def checkWeather():
-    weatherURL()
-
-def menu(i):
-    menuOptions = {
-        1: "Add title",
-        2: "Set start and end date",
-        3: "Invite people",
-        4: "Add description",
-        5: "Check weather",
-        6: "Finish and create an event",
-        7.: "Exit"
-    }
-
+    if choice == 1:
+        title()
+    elif choice == 2:
+        dates()
+    elif choice == 3:
+        attendees()
+    elif choice == 4:
+        description()
+    elif choice == 5:
+        weatherURL()
+    elif choice == 6:
+        if event['start'] == None or event['end'] == None or event['summary'] == None or event['attendees'] == None or \
+                event['description'] == None:
+            print("You need to fill every menu option!")
+            showMenu()
+        else:
+            create_event(event['start'], event['end'], event['summary'], event['attendees'], event['description'])
+        showMenu()
+    elif choice == 7:
+        exit()
+    else:
+        print("Wrong option. Choose again.")
+        showMenu()
